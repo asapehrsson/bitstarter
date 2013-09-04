a3948211eec7fb694d6f14ca5e343371e7392993
Aasa Pehrsson <asa.pehrsson@teliasonera.com>
2013-09-04 18:51:12 +0200

    minor adjustments

--- web.js
+++ web.js
@@ -9,6 +9,7 @@ app.get('/', function(request, response) {
     var data = fs.readFileSync(f);
     response.send(data.toString());
 });
+app.get('*', express.static(__dirname));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
